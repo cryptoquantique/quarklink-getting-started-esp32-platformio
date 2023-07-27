481b63f8e4a37b1546f372415edb0cc99b3d0286
Claudia Mura <CMura@cryptoquantique.com>
2023-07-27 20:05:43 +0100

    fixed comments

--- src/main.c
+++ src/main.c
@@ -63,11 +63,11 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         esp_wifi_connect();
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        ESP_LOGI(TAG,"Connection to the AP failed");
+        ESP_LOGI(TAG, "Connection to the AP failed");
         if (s_retry_num < 10) {
             esp_wifi_connect();
             s_retry_num++;
-            ESP_LOGI(TAG, "Retry to connect to the AP(%d/%d)", s_retry_num, 10);
+            ESP_LOGI(TAG, "Retry to connect to the AP (%d/%d)", s_retry_num, 10);
         } else {
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         }
